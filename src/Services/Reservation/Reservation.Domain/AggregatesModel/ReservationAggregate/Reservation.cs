5fe42d6d3059f57155bcb6e41a5295afd7c17425
Mani Duraisamy <mani.duraisamy@softwaregroup.com>
2020-07-19 21:43:03 +0100

    modify reservation model

--- src/Services/Reservation/Reservation.Domain/AggregatesModel/ReservationAggregate/Reservation.cs
+++ src/Services/Reservation/Reservation.Domain/AggregatesModel/ReservationAggregate/Reservation.cs
@@ -4,6 +4,8 @@ using System.Text;
 using Reservation.Domain.SharedKernel;
 using Reservation.Domain.AggregatesModel.MeetingRoomAggregate;
 using Reservation.Domain.SeedWork;
+using System.Linq;
+using Reservation.Domain.Exceptions;
 
 namespace Reservation.Domain.AggregatesModel.ReservationAggregate
 {
@@ -15,15 +17,39 @@ namespace Reservation.Domain.AggregatesModel.ReservationAggregate
         private int _employeeId;
         public int EmployeeId => _employeeId;
 
+        private DateTime _reservationDate;
+        public DateTime ReservationDate => _reservationDate;
+
+        private TimeSpan _startTime;
+        public TimeSpan StartTime => _startTime;
+
+        private TimeSpan _endTime;
+        public TimeSpan EndTime => _endTime;
+
         private readonly List<MovableResource> _movableResources = new List<MovableResource>();
         public IReadOnlyCollection<MovableResource> MovableResources => _movableResources;
 
         protected Reservation() { }
 
-        public Reservation(int meetingRoomId, int employeeId, List<MovableResource> resources)
+        public Reservation(int meetingRoomId,
+            int employeeId,
+            DateTime reservationDate,
+            TimeSpan startTime,
+            TimeSpan endTime)
         {
             _meetingRoomId = meetingRoomId;
             _employeeId = employeeId;
+            _reservationDate = reservationDate.Date;
+            _startTime = startTime;
+            _endTime = endTime;
+        }
+
+        public void AddResource(MovableResource movableResource)
+        {
+            if (_movableResources.Where(r => r.ResourceType == movableResource.ResourceType).Count() > 0)
+                throw new ReservationDomainException("Cannot add duplicate resources");
+
+            _movableResources.Add(movableResource);
         }
     }
 }
